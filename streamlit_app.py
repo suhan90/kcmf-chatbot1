622764b54e68e59f77f440f78181ede0bb60c35c
suhan90 <65432813+suhan90@users.noreply.github.com>
2025-03-05 16:50:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import requests
 import os
 
 # API 엔드포인트 (Cloud Functions 또는 Cloud Run에 배포된 주소)
-# API_ENDPOINT = os.environ.get("API_ENDPOINT", "https://......run.app/query")
+API_ENDPOINT = os.environ.get("API_ENDPOINT", "https://kcmf-chatbot-api.....run.app/un.app/query")
 
 st.title("문서 질의응답 시스템")
+st.write(API_ENDPOINT)
 st.write("문서 관련 질문을 입력하면 관련 문서에서 답변을 찾아드립니다.")
 
 # 사용자 입력
